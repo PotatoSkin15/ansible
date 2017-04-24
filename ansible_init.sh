5a0b4a685177dba38992476d91f1eaa5aef07ec1
Peck <kepeck1@gssla16070225.ndc.nasa.gov>
2017-04-24 15:43:34 -0400

    Added ansible_init script

---
+++ ansible_init.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Ansible project init script
+# KP, 4/21/17
+# v1.1
+
+clear
+
+# Prompt for project name
+echo 'What is the project name?'
+read project
+
+# Prompt for server role
+echo 'What is the server role?'
+read role
+
+while true; do
+
+  # Bonus: init git repo if one exists
+  echo 'Does a git repo exist? [y/n]'
+  read exist
+
+  case "$exist" in
+    y|Y)
+      echo 'Paste URL to git repo'
+      read repo
+      break
+    ;;
+    n|N)
+      break
+    ;;
+    *)
+      echo 'Enter valid option'
+    ;;
+  esac
+done
+
+# Create Ansible vars
+a_base=~/ansible
+a_tree=(defaults files handlers tasks vars)
+p_base=$a_base/$project
+r_base=$p_base/$role
+
+if [[ ! -e "$a_base" ]]; then
+  mkdir $a_base
+fi
+
+mkdir -p $r_base
+mkdir $p_base/logs
+
+# Create project base var
+p_base=$a_base/$project
+
+# Create base ansible.cfg file
+echo "[defaults]
+pipelining=True
+log_path=./logs/$project.log" > $p_base/ansible.cfg
+
+# Create directory tree
+for dir in ${a_tree[@]}; do
+  mkdir -p $r_base/$dir
+  touch $r_base/$dir/main.yml
+done
+
+if [[ -n "$repo" ]]; then
+  cd $p_base
+  git init
+  git remote add origin $repo
+  git add -A
+  git commit -m "Initial commit"
+  git push origin master:master
+  echo "Initial push to git done"
+fi
+
+echo "Done. Project initialized in $p_base"
